9dd5c6d80df064f68bf3101f0e5dd72db987b95e
sergio <sergio@Mac-mini-de-sergio.local>
2019-04-24 08:01:43 +0200

    modificar carpeta apuntes drupal

---
+++ Apuntes Drupal/renderizar blockes drupal 8.php
@@ -0,0 +1,9 @@
+<?php
+
+//Renderizar bloque en pantalla
+
+{{ drupal_entity('block_content', *id del bloque*) }}
+
+	//Ejemplo (renderizar bloque con ID 13:
+
+	{{ drupal_entity('block_content', 13 }}
