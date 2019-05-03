8fe08f42e7bbbbce86a97d688d55e3b9f59bf09c
sergio <sergio@Mac-mini-de-sergio.local>
2019-05-03 15:25:55 +0200

    modificado renderizar block drupal 8

--- Apuntes Drupal/renderizar blockes drupal 8.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-//Renderizar bloque en pantalla
-
-{{ drupal_entity('block_content', *id del bloque*) }}
-
-	//Ejemplo (renderizar bloque con ID 13:
-
-	{{ drupal_entity('block_content', 13 }}
