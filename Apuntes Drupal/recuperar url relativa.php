46230ed3a63dae42eb6dc5cb9ee363244afa9e38
Drauta <drauta@drauta.com>
2019-06-18 10:32:49 +0200

    edit recuperar url relativa

--- Apuntes Drupal/recuperar url relativa.php
+++ Apuntes Drupal/recuperar url relativa.php
@@ -15,4 +15,20 @@
 
 				//Resultado: public://2019-05/Modulo_2_apadrina_F41556.jpg
 
+	//Recuperar la url relativa de un archivo en Drupal 7
+
+			//Con file_create_url
+
+				file_create_url($row->field_field_imatge['0']['raw']['uri']);
+				
+				//Resultado: http://coacdev.drautadev.com/sites/default/files/foto-vestibul-1080-marca.png	
+
+
+			//Sin file_create_url
+
+				$row->field_field_imatge['0']['raw']['uri'];
+
+				//Resultado: public://foto-vestibul-1080-marca.png
+	
+
 ?>
